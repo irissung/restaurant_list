04ebd48eed159dee73d39d98f90cfe999a408b05
Iris <janyshop@msn.com>
2020-05-17 13:39:23 +0800

    feat: add body-parser and change about detail

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 
+//導入body parser
+const bodyParser = require('body-parser')
 //設定mongoose
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,6 +23,7 @@ const Restaurant = require('./models/restaurant')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   //從資料庫抓取餐廳資訊
@@ -30,9 +33,12 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
